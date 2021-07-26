4198fd8f087ce3f6dbda337ece070d7246f2a370
gyullo18 <gold798@naver.com>
2021-07-26 14:58:38 +0900

    7-26 1ban project2 1

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = [
+from django.urls import path
+#7/26
+from profileapp.views import ProfileCreateView
 
+app_name = 'profileapp'
+
+urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create')#어떤 주소로 접근하는지, 로직(as view), name
 ]
+#라우팅 끝 --- > profileapp에 템플릿 디렉토리 생성 >> profile디렉토리 생성 -> html파일
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+#7/26
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    #뭘 만들건지
+    model = Profile #프로필 앱안의 모델
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:gyullo')#다 만들고 어디로 돌아갈지 #콜론 앞의 accountapp - app_name이 들어감
+    template_name = 'profileapp/create.html'#프로필 생성페이지를 어떤html로 사용할지
+
+    #라우팅 -- profileapp > urls
