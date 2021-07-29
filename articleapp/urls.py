5311e08ff5a9eb3a79ffc0612223607ac7a81459
gyullo18 <gold798@naver.com>
2021-07-29 15:59:07 +0900

    7-29 1ban project2 4

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,6 +1,11 @@
 #7/29 터미널 - python managy.py startapp articleapp
 #메인의 settings의 installed에 articleapp추가
 #메인 urls에 경로 추가 후 articleapp에 urls파일 생성
-urlpatterns = [
+from django.urls import path
+from django.views.generic import TemplateView
 
+app_name = 'articleapp'
+
+urlpatterns = [
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html', name='list'))
 ]
