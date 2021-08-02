15d7aa585a3d708444c39a850c3db01d6b331e00
gyullo18 <gold798@naver.com>
2021-08-02 15:48:17 +0900

    8/2 1ban project2 3

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -4,8 +4,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from articleapp.views import ArticleCreateView
+
 app_name = 'articleapp'
 
 urlpatterns = [
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list')
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+    #8/2 하고 create.html로
+    path('create/', ArticleCreateView.as_view(), name='create'),
+
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#8/2
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('accountapp:gyullo')
+    template_name = 'articleapp/create.html'
+
+    #url 라우팅
+    #create.html완성 후
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
