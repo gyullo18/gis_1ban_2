5b982d7666cbbecdde803a96aa86633de5f50961
gyullo18 <gold798@naver.com>
2021-08-09 14:45:20 +0900

    8/9 1ban project2 1

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,4 +1,11 @@
+#8/5 댓글구현
+from django.urls import path
+
+from commentapp.views import CommentCreateView
+
+app_name = 'commentapp'
 
 urlpatterns = [
-    
+    #8/9 라우팅후 template > commentapp> create
+    path('create/', CommentCreateView.as_view(), name='create'),
 ]
--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from commentapp.forms import CommentCreationForm
+from commentapp.models import Comment
+
+#8/9
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentapp/create.html'
+    #4분정도
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs = {'pk':self.object.article.pk})
+    #url 연결
