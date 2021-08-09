c1fe1f45eaea68247304be5a806478461b8e584f
gyullo18 <gold798@naver.com>
2021-08-09 15:43:53 +0900

    8/9 1ban project2 5

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,11 +1,13 @@
 #8/5 댓글구현
 from django.urls import path
 
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'
 
 urlpatterns = [
     #8/9 라우팅후 template > commentapp> create
     path('create/', CommentCreateView.as_view(), name='create'),
+    #8/9 deleteview라우팅 후 delete.html
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete'),
 ]
--- commentapp/views.py
+++ commentapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
@@ -26,3 +26,13 @@ class CommentCreateView(CreateView):
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
     # url 연결
+
+# 8/9
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+    # url 라우팅
