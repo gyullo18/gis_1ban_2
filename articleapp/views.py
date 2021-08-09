52aba925485b38c6e3b4c350424bcdb68c1e4abc
gyullo18 <gold798@naver.com>
2021-08-09 14:59:49 +0900

    8/9 1ban project2 2

--- articleapp/views.py
+++ articleapp/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
+from django.views.generic.edit import FormMixin
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -14,6 +15,9 @@ from articleapp.models import Article
 
 #8/4
 #로그인이 필요하다라는 인증과정 -- 장고 기본 제공
+from commentapp.forms import CommentCreationForm
+
+
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
 class ArticleCreateView(CreateView):
@@ -33,8 +37,11 @@ class ArticleCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 #8/2 readview
-class ArticleDetailView(DetailView):
+#8/9 Mixin추가
+class ArticleDetailView(DetailView, FormMixin):
     model = Article
+    # creat 뷰처럼 뭔가를 만들수 있는 기능 할당됨 -- mixin으로 인해
+    form_class = CommentCreationForm
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
     #url 라우팅
