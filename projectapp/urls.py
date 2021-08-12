9bc5dae597b0aa6d1e19b9be4afcc587a4620f07
gyullo18 <gold798@naver.com>
2021-08-12 16:27:18 +0900

    8/12 1ban project2 7

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,7 +1,7 @@
 #8/12
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectDetailView
 
 app_name = 'projectapp'
 
@@ -10,4 +10,6 @@ app_name = 'projectapp'
 urlpatterns = [
     #8/12 라우팅 후 create.html만들기
     path('create/', ProjectCreateView.as_view(), name='create'),
+    #8/12 라우팅 후 detail.html만들기
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -16,3 +16,11 @@ class ProjectCreateView(CreateView):
     success_url = reverse_lazy('articleapp:list')
     template_name = 'projectapp/create.html'
     # 로직 만들고 url에 라우팅
+
+# 8/12
+#detailview
+class ProjectDetailView(DetailView):
+    model = Project
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
+    #url에 라우팅
