6aa5efabb4be7919f9cd1e8d84b02de9ee698340
gyullo18 <gold798@naver.com>
2021-08-18 09:09:29 +0900

    8/18 1ban project2 1

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,21 +1,30 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
 
+#8/18 인증과정 -- 로그인 한 사람만 이용 가능하게
 #8/12 forms.py 작성 후 CD/L 시작
 #createview
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
     success_url = reverse_lazy('articleapp:list')
     template_name = 'projectapp/create.html'
     # 로직 만들고 url에 라우팅
+
+    # 8/18 5분
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk': self.object})
 
 # 8/12
 #detailview
