df8742d9227b3e95298b56a5d0467e0c82453849
gyullo18 <gold798@naver.com>
2021-09-02 17:34:16 +0900

    9/2 1ban project2 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -6,11 +6,9 @@ from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdate
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('Login/', LoginView.as_view(template_name='accountapp/Login.html'),
-         name='login'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
 
-    path('Logout/', LogoutView.as_view(template_name='accountapp/Logout.html'),
-         name='logout'),
+    path('logout/', LogoutView.as_view(), name='Logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),#클래스를 함수로 뱉어주는 메서드 #라우트
 
